7327f0b863f62dca0a3b2eaf6d6b1d4ff26bd72e
Alexis Fuchs <afuchs@student.42mulhouse.fr>
2022-04-06 12:50:16 +0200

    open_win060420221250

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: afuchs <afuchs@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 14:57:42 by afuchs            #+#    #+#             */
-/*   Updated: 2022/04/06 12:23:36 by afuchs           ###   ########.fr       */
+/*   Updated: 2022/04/06 12:48:54 by afuchs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/libft.h"
@@ -24,13 +24,22 @@ void	esc_win(int	keycode, t_win *scr)
 	}
 }
 
+t_win	*open_win()
+{
+	t_win	*scr;
+
+	scr = ft_calloc(1, sizeof (t_win));
+	scr->cid = mlx_init();
+	scr->wid = mlx_new_window(scr->cid, 600, 500, "so_long");
+	return (scr);
+}
+
 int	main(void)
 {
-	t_win	scr;
+	t_win	*scr;
 
-	scr.cid = mlx_init();
-	scr.wid = mlx_new_window(scr.cid, 840, 380, "so_long");
-	mlx_key_hook(scr.wid, ((int (*)())&esc_win), &scr);
-	mlx_loop(scr.cid);
+	scr = open_win();
+	mlx_key_hook(scr->wid, ((int (*)())&esc_win), scr);
+	mlx_loop(scr->cid);
 	return (0);
 }
